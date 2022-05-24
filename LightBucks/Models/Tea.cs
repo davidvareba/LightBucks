b3e42ae88aeee7387fcda64542dedf2374587ab8
david vareba <davidvareba@gmail.com>
2022-05-23 19:37:07 -0500

    completed Models/classes

---
+++ LightBucks/Models/Tea.cs
@@ -0,0 +1,13 @@
+﻿namespace LightBucks.Models
+{
+    public class Tea
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public int TeaId { get; set; }
+
+    }
+}
