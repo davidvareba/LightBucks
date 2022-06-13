ef734683de29da8bb443b82a51140972e127662c
david vareba <davidvareba@gmail.com>
2022-06-13 15:07:35 -0500

    repositories

--- LightBucks/Models/Coffee.cs
+++ LightBucks/Models/Coffee.cs
@@ -5,8 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
-        public string Description { get; set; }
+        public string Descriptions { get; set; }
         public string ImgUrl { get; set; }
         public int CoffeeId { get; set; }
+        public int UserId { get; set; }
     }
 }
---
+++ LightBucks/Repositories/ISnackRepository.cs
@@ -0,0 +1,17 @@
+﻿using LightBucks.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace LightBucks.Repositories
+{
+    public interface ISnackRepository
+    {
+        List<Snack> GetAllSnack();
+        Snack GetSnackById(int id);
+        List<Snack> GetSnackByUserId(int id);
+        public void AddSnack(Snack snack);
+        public void UpdateSnack(Snack snack);
+        public void DeleteSnack(int id);
+    }
+}
